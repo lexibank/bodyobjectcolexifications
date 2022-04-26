753e83226225c45a9a7d95f70aa1e71b8278758b
Johannes Englisch <johannes_englisch@eva.mpg.de>
2022-04-26 10:46:08 +0200

    idempotency

--- cldfbench_tjukabodyobject.py
+++ cldfbench_tjukabodyobject.py
@@ -292,6 +292,11 @@ class Dataset(BaseDataset):
         with self.cldf_writer(args) as writer:
             self._schema(writer)
 
+            features.sort(key=lambda f: f['ID')
+            codes.sort(key=lambda c: c['ID'])
+            languages.sort(key=lambda l: l['ID'])
+            values.sort(key=lambda v: v['ID'])
+
             writer.objects['ParameterTable'] = features
             writer.objects['CodeTable'] = codes
             writer.objects['LanguageTable'] = languages
